ba347e3660f2f3eca813d57c95c0117c1635e57a
Devansh0109 <157219765+Devansh0109@users.noreply.github.com>
2026-03-13 18:53:32 +0000

    added Admin Configuration

--- src/movies/admin.py
+++ src/movies/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Movie
 
-# Register your models here.
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ("title", "genre", "release_year", "director", "created_at")
+    search_fields = ("title","genre", "director")
+    list_filter = ("genre", "release_year")
