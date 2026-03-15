e37de700535aeec037089368a6d926b62fc41736
Devansh0109 <157219765+Devansh0109@users.noreply.github.com>
2026-03-15 12:06:58 +0000

    Added endpoint for retrieving a single movie

--- src/movies/views.py
+++ src/movies/views.py
@@ -1,5 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .models import Movie
 from .serializers import MovieSerializer
 
@@ -7,4 +8,14 @@ from .serializers import MovieSerializer
 def movie_list(request):
     movies = Movie.objects.all()
     serializer = MovieSerializer(movies, many=True)
+    return Response(serializer.data)
+
+@api_view(["GET"])
+def movie_detail(request, pk):
+    try:
+        movie = Movie.objects.get(pk=pk)
+    except Movie.DoesNotExist:
+        return Response({"error: movie not found"}, status=status.HTTP_404_NOT_FOUND)
+    
+    serializer = MovieSerializer(movie)
     return Response(serializer.data)
