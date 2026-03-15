80d8887c01c56db2707c3e119c4ab919864f3e0a
Devansh0109 <157219765+Devansh0109@users.noreply.github.com>
2026-03-15 18:49:54 +0000

    added admin review

--- src/movies/admin.py
+++ src/movies/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from .models import Movie
+from .models import Movie, Review
 
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
     list_display = ("title", "genre", "release_year", "director", "created_at")
     search_fields = ("title","genre", "director")
     list_filter = ("genre", "release_year")
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ("movie", "reviewer_name", "rating", "created_at")
+    search_fields = ("movie__title", "reviewer_name")
+    list_filter = ("rating", "created_at")
