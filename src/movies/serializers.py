c6e97cee6b341e768729ae236af012100f0c41ac
Devansh0109 <157219765+Devansh0109@users.noreply.github.com>
2026-03-15 20:39:49 +0000

    added average rating and review count

--- src/movies/serializers.py
+++ src/movies/serializers.py
@@ -1,10 +1,23 @@
 from rest_framework import serializers
+from django.db.models import Avg
 from .models import Movie, Review
 
 class MovieSerializer(serializers.ModelSerializer):
+    average_rating = serializers.SerializerMethodField()
+    review_count = serializers.SerializerMethodField()
     class Meta:
         model = Movie
-        fields = ['title', 'genre', 'release_year', 'director', 'overview', 'created_at']
+        fields = ['title', 'genre', 'release_year', 'director', 'overview','average_rating', 'review_count', 'created_at']
+    
+    def get_average_rating(self, obj):
+        avg = obj.reviews.aggregate(avg_rating=Avg('rating'))['avg_rating']
+        if avg is None:
+            return None
+        else:
+            return round(avg, 2)
+    
+    def get_review_count(self, obj):
+        return obj.reviews.count()
 
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
