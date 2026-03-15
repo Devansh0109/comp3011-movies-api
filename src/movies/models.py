1b8b40ab6aa86bd36b80af6f465bf3a47d300187
Devansh0109 <157219765+Devansh0109@users.noreply.github.com>
2026-03-15 18:48:28 +0000

    created a new model for reviews

--- src/movies/models.py
+++ src/movies/models.py
@@ -10,3 +10,13 @@ class Movie(models.Model):
 
     def __str__(self):
         return f"{self.title}({self.release_year})"
+    
+class Review(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name="reviews")
+    reviewer_name = models.CharField(max_length=100)
+    rating = models.PositiveIntegerField()
+    comment = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Review for {self.movie.title} by {self.reviewer_name}"
