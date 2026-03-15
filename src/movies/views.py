97a0d89903de6a2c86d3a20a62cdcc120f047559
Devansh0109 <157219765+Devansh0109@users.noreply.github.com>
2026-03-15 12:49:41 +0000

    Added POST for movies via JSON

--- src/movies/views.py
+++ src/movies/views.py
@@ -1,14 +1,23 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+
 from .models import Movie
 from .serializers import MovieSerializer
 
-@api_view(["GET"])
+@api_view(["GET", "POST"])
 def movie_list(request):
-    movies = Movie.objects.all()
-    serializer = MovieSerializer(movies, many=True)
-    return Response(serializer.data)
+    if request.method == "GET":
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+        return Response(serializer.data)
+    else:
+        serializer = MovieSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(["GET"])
 def movie_detail(request, pk):
