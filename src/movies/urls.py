6bbf9bda0f9302f242079fa7bf3e49efd6553dc6
Devansh0109 <157219765+Devansh0109@users.noreply.github.com>
2026-03-19 05:53:49 +0000

    added url for user register and login

--- src/movies/urls.py
+++ src/movies/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views import (
+    register,
+    login_view,
     movie_list, 
     movie_detail, 
-    movie_review, 
+    movie_review,
+    review_detail, 
     movie_search,
     top_rated_movies,
     most_reviewed_movies,
@@ -10,9 +13,12 @@ from .views import (
     )
 
 urlpatterns = [
+    path("auth/register/", register, name="register"),
+    path("auth/login/", login_view, name="login"),    
     path("movies/", movie_list, name="movie-list"),
     path("movies/<int:pk>/", movie_detail, name="movie-detail"),
     path("movies/<int:pk>/reviews/", movie_review, name="movie-reviews"),
+    path("movies/<int:pk>/reviews/<int:review_pk>/", review_detail, name="review-detail"),
     path("movies/search/", movie_search, name="movie-search"),
     path("movies/top-rated/", top_rated_movies, name="top-rated-movies"),
     path("movies/most-reviewed/", most_reviewed_movies, name="most-reviewed-movies"),
